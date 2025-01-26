c972684ae872bc396393a405f9554fc18b178785
Mateusz Saczawa <sacz.mateusz@gmail.com>
2025-01-26 15:24:23 +0100

    Add MongoDB integration, rewrite User CRUD

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Expense Tracker Backend
 
 Application based on Node & Express & MongoDB
+
+### .env example
+
+PORT=3000  
+DATABASE_URL=mongodb://localhost:27017/expense-tracker
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,26 @@
 import express, { Request, Response } from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 import userRoutes from "./routes/users";
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+const databaseUrl = process.env.DATABASE_URL || "";
 
 app.use(express.json());
+
+mongoose
+  .connect(databaseUrl)
+  .then(() => {
+    console.log("Database connected.");
+  })
+  .catch((err) => {
+    console.log("Could not connect to the database", err);
+    process.exit();
+  });
+
 app.use("/users", userRoutes);
 
 app.get("/", (req: Request, res: Response) => {
